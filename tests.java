167646eeaa9f46d54c4b61a8f3dd7a82829098de
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-01-23 16:35:20 +0100

    feat: Create files
    Create files with template classes and main function for main program, test program and shell script.
    
    close #17

---
+++ tests.java
@@ -0,0 +1,11 @@
+public class Tests {
+    public static void main(String[] args) {
+        System.out.println("Tests!");
+        testLic0();
+    }
+
+    // @Test
+    public static void testLic0() {
+        System.out.println("Test lic0");
+    }
+}
