6f21bcceee087e2dae5c108c762a6cfb72bae2fe
Philip Ågren Jahnsson <philipaj@kth.se>
2025-01-24 16:17:00 +0100

    feat: Add LIC0 method and added maven project for tests.
    Added **lic0()** method to the main java file, as well as creating a maven project for the tests. Also added tests for **lic()** method.
    
    close #2
    
    Co-authored-by: linda.nycander@gmail.com <linda.nycander@gmail.com>
    Co-authored-by: Elin Fransholm <efransholm@users.noreply.github.com>

---
+++ lic/src/main/java/com/group7/Launch_interceptor.java
@@ -0,0 +1,63 @@
+package com.group7;
+
+public class Launch_interceptor {
+
+    // Define all global variables
+    public int NUMPOINTS;
+    public double[][] POINTS;
+
+    public record ParameterRecord(
+            double LENGTH1,
+            double RADIUS1,
+            double EPSILON,
+            double AREA1,
+            int Q_PTS,
+            int QUADS,
+            int N_PTS,
+            double DIST,
+            int K_PTS,
+            int A_PTS,
+            int B_PTS,
+            int C_PTS,
+            int D_PTS,
+            int E_PTS,
+            int F_PTS,
+            int G_PTS,
+            double LENGTH2,
+            double RADIUS2,
+            double AREA2) {
+
+    }
+    public ParameterRecord PARAMETERS;
+
+    public String[][] LCM;
+    public boolean[] PUV;
+
+    public Launch_interceptor(int numpoints, double[][] points, ParameterRecord parameters, String[][] lcm, boolean[] puv) {
+        this.NUMPOINTS = numpoints;
+        this.POINTS = points;
+        this.PARAMETERS = parameters;
+        this.LCM = lcm;
+        this.PUV = puv;
+    }
+
+
+
+    public static void main(String[] args) {
+        System.out.println("Launch interceptor program!");
+    }
+
+    public boolean lic0() {
+        boolean state = false;
+        for (int i = 1; i < this.NUMPOINTS; i++) {
+            double[] point1 = this.POINTS[i - 1];
+            double[] point2 = this.POINTS[i];
+            // calculate the euclidian distance between two points
+            double distance = Math.sqrt(Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2));
+            if (distance > this.PARAMETERS.LENGTH1) {
+                state = true;
+            }
+        }
+        return state;
+    }
+}
---
+++ lic/src/test/java/TestLic.java
@@ -0,0 +1,33 @@
+import com.group7.Launch_interceptor;
+import com.group7.Launch_interceptor.ParameterRecord;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.Test;
+
+public class TestLic {
+
+    @Test
+    void testLic0_true() {
+        // Case 1, we know to be true according to input variables
+        System.out.println("Test lic0");
+        int numpoints = 3;
+        double[][] points = {{0,3},{1,5},{3,8}};
+        ParameterRecord parameters = new ParameterRecord(3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        Launch_interceptor lic = new Launch_interceptor(numpoints,points,parameters,lcm,puv);
+        assertTrue(lic.lic0());
+    }
+
+    @Test
+    void testLic_false() {
+        // Case 2, we know to be false according to input variables
+        System.out.println("Test lic0");
+        int numpoints = 3;
+        double[][] points = {{-2,3},{1,-5},{-3,8}};
+        ParameterRecord parameters = new ParameterRecord(50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        Launch_interceptor lic = new Launch_interceptor(numpoints,points,parameters,lcm,puv);
+        assertFalse(lic.lic0());
+    }
+}
