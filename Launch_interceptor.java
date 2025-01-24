3f8dae4af9d363308faaee1447714f38e2a8e47a
Philip Ågren Jahnsson <philipaj@kth.se>
2025-01-24 11:56:33 +0100

    feat: Add global variables Defined all global input variables available to DECIDE() method.
    close #23

--- Launch_interceptor.java
+++ Launch_interceptor.java
@@ -1,4 +1,58 @@
+
+
 public class Launch_interceptor {
+
+    // Define all global variables
+    public static int NUMPOINTS;
+    public static int[][] POINTS;
+    public record ParameterRecord(
+        double LENGTH1,
+        double RADIUS1,
+        double EPSILON,
+        double AREA1,
+        int Q_PTS,
+        int QUADS,
+        int N_PTS,
+        double DIST,
+        int K_PTS,
+        int A_PTS,
+        int B_PTS,
+        int C_PTS,
+        int D_PTS,
+        int E_PTS,
+        int F_PTS,
+        int G_PTS,
+        double LENGTH2,
+        double RADIUS2,
+        double AREA2
+    ){}
+    public static ParameterRecord PARAMETERS = new ParameterRecord(
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0
+        );
+
+    public static String[][] LCM = new String[15][15];
+    public static boolean[] PUV = new boolean[15];
+
+
+
     public static void main(String[] args) {
         System.out.println("Launch interceptor program!");
     }
