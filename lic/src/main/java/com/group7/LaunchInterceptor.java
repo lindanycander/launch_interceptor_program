1053e804c8e3fb610ca091bb8e6c5cdd60237707
lindanycander <76840066+lindanycander@users.noreply.github.com>
2025-01-26 22:23:03 +0100

    feat: Implement lic2
    Implement lic2 and some tests for it.
    
    closes #4

--- lic/src/main/java/com/group7/LaunchInterceptor.java
+++ lic/src/main/java/com/group7/LaunchInterceptor.java
@@ -6,28 +6,12 @@ public class LaunchInterceptor {
     public int NUMPOINTS;
     public double[][] POINTS;
 
-    public record ParameterRecord(
-            double LENGTH1,
-            double RADIUS1,
-            double EPSILON,
-            double AREA1,
-            int Q_PTS,
-            int QUADS,
-            int N_PTS,
-            double DIST,
-            int K_PTS,
-            int A_PTS,
-            int B_PTS,
-            int C_PTS,
-            int D_PTS,
-            int E_PTS,
-            int F_PTS,
-            int G_PTS,
-            double LENGTH2,
-            double RADIUS2,
-            double AREA2) {
+    public record ParameterRecord(double LENGTH1, double RADIUS1, double EPSILON, double AREA1, int Q_PTS, int QUADS,
+            int N_PTS, double DIST, int K_PTS, int A_PTS, int B_PTS, int C_PTS, int D_PTS, int E_PTS, int F_PTS,
+            int G_PTS, double LENGTH2, double RADIUS2, double AREA2) {
 
     }
+
     public ParameterRecord PARAMETERS;
 
     public String[][] LCM;
@@ -36,13 +20,14 @@ public class LaunchInterceptor {
     /**
      * The constructor for the Launch_interceptor class.
      * 
-     * @param numpoints Number of points.
-     * @param points Array of points.
+     * @param numpoints  Number of points.
+     * @param points     Array of points.
      * @param parameters Parameter record.
-     * @param lcm Launch criteria matrix.
-     * @param puv Preliminary unlocking vector.
+     * @param lcm        Launch criteria matrix.
+     * @param puv        Preliminary unlocking vector.
      */
-    public LaunchInterceptor(int numpoints, double[][] points, ParameterRecord parameters, String[][] lcm, boolean[] puv) {
+    public LaunchInterceptor(int numpoints, double[][] points, ParameterRecord parameters, String[][] lcm,
+            boolean[] puv) {
         this.NUMPOINTS = numpoints;
         this.POINTS = points;
         this.PARAMETERS = parameters;
@@ -50,10 +35,16 @@ public class LaunchInterceptor {
         this.PUV = puv;
     }
 
-
-
     public static void main(String[] args) {
         System.out.println("Launch interceptor program!");
+        System.out.println("Test lic2");
+        int numpoints = 3;
+        double[][] points = { { -5, 0 }, { 0, 0 }, { 5, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 0.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        lic.lic2();
     }
 
     public boolean lic0() {
@@ -64,6 +55,35 @@ public class LaunchInterceptor {
             // calculate the euclidian distance between two points
             double distance = Math.sqrt(Math.pow(point1[0] - point2[0], 2) + Math.pow(point1[1] - point2[1], 2));
             if (distance > this.PARAMETERS.LENGTH1) {
+                state = true;
+            }
+        }
+        return state;
+    }
+
+    public boolean lic2() {
+        boolean state = false;
+        for (int i = 2; i < this.NUMPOINTS; i++) {
+            double[] point1 = this.POINTS[i - 2];
+            double[] point2 = this.POINTS[i - 1];
+            double[] point3 = this.POINTS[i];
+            double[] vector1 = { point2[0] - point1[0], point2[1] - point1[1] };
+            double[] vector2 = { point3[0] - point2[0], point3[1] - point2[1] };
+            double dotProduct = vector1[0] * vector2[0] + vector1[1] * vector2[1];
+            double magnitude1 = Math.sqrt(Math.pow(vector1[0], 2) + Math.pow(vector1[1], 2));
+            double magnitude2 = Math.sqrt(Math.pow(vector2[0], 2) + Math.pow(vector2[1], 2));
+
+            // Skip if either vector has zero magnitude
+            if (magnitude1 == 0 || magnitude2 == 0) {
+                continue;
+            }
+            // Skip if any point coincides the vertex
+            if (point1[0] == point2[0] && point1[1] == point2[1] || point2[0] == point3[0] && point2[1] == point3[1]) {
+                continue;
+            }
+
+            double angle = Math.acos(dotProduct / (magnitude1 * magnitude2));
+            if (angle < (Math.PI - this.PARAMETERS.EPSILON) || angle > (Math.PI + this.PARAMETERS.EPSILON)) {
                 state = true;
             }
         }
--- lic/src/test/java/TestLic.java
+++ lic/src/test/java/TestLic.java
@@ -11,24 +11,90 @@ public class TestLic {
         // Case 1, we know to be true according to input variables
         System.out.println("Test lic0");
         int numpoints = 3;
-        double[][] points = {{0,3},{1,5},{3,8}};
-        ParameterRecord parameters = new ParameterRecord(3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        double[][] points = { { 0, 3 }, { 1, 5 }, { 3, 8 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         String[][] lcm = new String[15][15];
         boolean[] puv = new boolean[15];
-        LaunchInterceptor lic = new LaunchInterceptor(numpoints,points,parameters,lcm,puv);
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
         assertTrue(lic.lic0());
     }
 
     @Test
-    void testLic_false() {
+    void testLic0_false() {
         // Case 2, we know to be false according to input variables
         System.out.println("Test lic0");
         int numpoints = 3;
-        double[][] points = {{-2,3},{1,-5},{-3,8}};
-        ParameterRecord parameters = new ParameterRecord(50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        double[][] points = { { -2, 3 }, { 1, -5 }, { -3, 8 } };
+        ParameterRecord parameters = new ParameterRecord(50, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
         String[][] lcm = new String[15][15];
         boolean[] puv = new boolean[15];
-        LaunchInterceptor lic = new LaunchInterceptor(numpoints,points,parameters,lcm,puv);
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
         assertFalse(lic.lic0());
     }
+
+    @Test
+    void testLic2_lessThanPi_true() {
+        // Case 1, we know to be true according to input variables
+        System.out.println("Test lic1");
+        int numpoints = 3;
+        double[][] points = { { 0, 5 }, { 0, 0 }, { 5, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 0.3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertTrue(lic.lic2());
+    }
+
+    @Test
+    void testLic2_greaterThanPi_true() {
+        // Case 3, we know to be true according to input variables
+        System.out.println("Test lic2");
+        int numpoints = 3;
+        double[][] points = { { -1, -1 }, { 0, 0 }, { 1, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 0.2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertTrue(lic.lic2());
+    }
+
+    @Test
+    void testLic2_false() {
+        // Case 2, we know to be false according to input variables
+        System.out.println("Test lic2");
+        int numpoints = 3;
+        double[][] points = { { 0, 0.5 }, { 0, 0 }, { 0.5, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertFalse(lic.lic2());
+    }
+
+    @Test
+    void testLic2_skip_false() {
+        // Case 5, we know to be false because one point is equal to the vertex-point
+        System.out.println("Test lic2");
+        int numpoints = 3;
+        double[][] points = { { 0, 0 }, { 0, 0 }, { 1, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertFalse(lic.lic2());
+    }
+
+    @Test
+    void testLic2_parallel_false() {
+        // Case 6, we know to be false since the vectors are parallel
+        System.out.println("Test lic2");
+        int numpoints = 3;
+        double[][] points = { { 0, 0 }, { 2, 0 }, { 1, 0 } };
+        ParameterRecord parameters = new ParameterRecord(3, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertFalse(lic.lic2());
+    }
+
 }
