35b1ffe7f75e0ac253a3301a5a5fc7ecb8a17e4e
Philip Ågren Jahnsson <philipaj@kth.se>
2025-01-27 09:02:20 +0100

    feat: Add lic3() method (#29)
    * feat: Add lic3() method
    
    Added lic3() method to check if area between 3 consecutive points are larger than some defined area AREA1. Also added tests for said method.
    
    close #5
    
    * Update: changes back testLic0_false to testLic_false
    
    Reverted changes to avoid merge conflicts
    
    * Update: TestLic.java
    
    Updated test function to test right method

--- lic/src/test/java/TestLic.java
+++ lic/src/test/java/TestLic.java
@@ -1,8 +1,9 @@
-import com.group7.LaunchInterceptor;
-import com.group7.LaunchInterceptor.ParameterRecord;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+
+import com.group7.LaunchInterceptor;
+import com.group7.LaunchInterceptor.ParameterRecord;
 
 public class TestLic {
 
@@ -30,5 +31,31 @@ public class TestLic {
         boolean[] puv = new boolean[15];
         LaunchInterceptor lic = new LaunchInterceptor(numpoints,points,parameters,lcm,puv);
         assertFalse(lic.lic0());
+    }
+
+    @Test
+    void testLic3_true() {
+        // Case 1, we know to be true according to input variables
+        System.out.println("Test lic3");
+        int numpoints = 3;
+        double[][] points = {{-2,3},{1,-5},{-3,8}};
+        ParameterRecord parameters = new ParameterRecord(0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints,points,parameters,lcm,puv);
+        assertTrue(lic.lic3());
+    }
+
+    @Test
+    void testLic3_false() {
+        // Case 2, we know to be false according to input variables
+        System.out.println("Test lic3");
+        int numpoints = 3;
+        double[][] points = {{-2,3},{1,-5},{-3,8}};
+        ParameterRecord parameters = new ParameterRecord(0,0,0,50,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints,points,parameters,lcm,puv);
+        assertFalse(lic.lic3());
     }
 }
