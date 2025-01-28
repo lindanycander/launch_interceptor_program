lindanycander/launch_interceptor_program

commit 50514e502240b3c00c07e21fd7aa7f4c76d40cb7

feat: implement LIC 8 and tests for it
implement LIC 8 and tests for this LIC

fixes #10

lic/src/main/java/com/group7/LaunchInterceptor.java
@@ -204,6 +204,57 @@ public class LaunchInterceptor {
         return state;
     }
 
+    public boolean lic8() {
+        // There exists at least one set of three data points separated by exactly A_PTS
+        // and B_PTS consecutive intervening points, respectively, that cannot be
+        // contained within or on a circle of radius RADIUS1. The condition is not met
+        // when NUMPOINTS < 5.
+
+        if (this.NUMPOINTS >= 5 && this.PARAMETERS.A_PTS >= 1 && this.PARAMETERS.B_PTS >= 1
+                && this.PARAMETERS.A_PTS + this.PARAMETERS.B_PTS <= this.NUMPOINTS - 3) {
+
+            // start from a point where we have enough previous points to compare to
+            int lastPointIndex = this.PARAMETERS.A_PTS + this.PARAMETERS.B_PTS + 2;
+
+            for (int i = lastPointIndex; i < this.NUMPOINTS; i++) {
+                // define the three points to compare
+                double[] point1 = this.POINTS[i - lastPointIndex];
+                double[] point2 = this.POINTS[i - this.PARAMETERS.B_PTS - 1];
+                double[] point3 = this.POINTS[i];
+
+                // calculate the sides of the triangle
+                double a = Math.sqrt(Math.pow(point2[0] - point1[0], 2) + Math.pow(point2[1] - point1[1], 2));
+                double b = Math.sqrt(Math.pow(point3[0] - point2[0], 2) + Math.pow(point3[1] - point2[1], 2));
+                double c = Math.sqrt(Math.pow(point1[0] - point3[0], 2) + Math.pow(point1[1] - point3[1], 2));
+
+                // Calculate the area of the triangle using the determinant formula
+                double area = Math.abs(point1[0] * (point2[1] - point3[1]) + point2[0] * (point3[1] - point1[1])
+                        + point3[0] * (point1[1] - point2[1])) / 2.0;
+
+                // If the area is zero, the points are collinear, so the circumcircle radius is
+                // infinite
+                if (area == 0) {
+                    double maxLength = Math.max(Math.max(a, b), c) / 2;
+                    if (maxLength >= this.PARAMETERS.RADIUS1) {
+                        return true; // Points are not within the radius
+                    }
+                    break;
+                }
+
+                // Calculate the circumcircle radius using the formula from
+                // https://artofproblemsolving.com/wiki/index.php/Circumradius
+                double circumcircleRadius = (a * b * c) / (4 * area);
+
+                // Check if the circumcircle radius is within the given radius
+                if (circumcircleRadius > this.PARAMETERS.RADIUS1) {
+                    return true;
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+
     public boolean lic9() {
         boolean state = false;
 

lic/src/test/java/TestLic.java
@@ -6,7 +6,6 @@ import com.group7.LaunchInterceptor;
 import com.group7.LaunchInterceptor.ParameterRecord;
 
 public class TestLic {
-
     @Test
     void testLic0_true() {
         // Case 1, we know to be true according to input variables
@@ -261,6 +260,51 @@ public class TestLic {
         boolean[] puv = new boolean[15];
         LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
         assertFalse(lic.lic7());
+    }
+
+    @Test
+    void testLic8_true() {
+        int numpoints = 5;
+        double[][] points = { { 0, 2 }, { 3, 15 }, { 0, 0 }, { -5, -5 }, { 2, 0 } };
+        double radius1 = 0.5;
+        int a_pts = 1;
+        int b_pts = 1;
+        ParameterRecord parameters = new ParameterRecord(0, radius1, 0, 0, 0, 0, 0, 0, 0, a_pts, b_pts, 0, 0, 0, 0, 0,
+                0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertTrue(lic.lic8());
+    }
+
+    @Test
+    void testLic8_onLine_false() {
+        int numpoints = 5;
+        double[][] points = { { 0, 0 }, { 0, 1 }, { 0, 2 }, { 0, 3 }, { 0, 4 } };
+        double radius1 = 3;
+        int a_pts = 1;
+        int b_pts = 1;
+        ParameterRecord parameters = new ParameterRecord(0, radius1, 0, 0, 0, 0, 0, 0, 0, a_pts, b_pts, 0, 0, 0, 0, 0,
+                0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertFalse(lic.lic8());
+    }
+
+    @Test
+    void testLic8_false() {
+        int numpoints = 6;
+        double[][] points = { { 0, 0 }, { 0, 1 }, { 1, 0 }, { 1, 1 }, { -1, 0 }, { -1, -1 } };
+        double radius1 = 2;
+        int a_pts = 1;
+        int b_pts = 1;
+        ParameterRecord parameters = new ParameterRecord(0, radius1, 0, 0, 0, 0, 0, 0, 0, a_pts, b_pts, 0, 0, 0, 0, 0,
+                0, 0, 0);
+        String[][] lcm = new String[15][15];
+        boolean[] puv = new boolean[15];
+        LaunchInterceptor lic = new LaunchInterceptor(numpoints, points, parameters, lcm, puv);
+        assertFalse(lic.lic8());
     }
 
     @Test

commit e89da1830b75bda41904e0224f390ca88dba0c9b

fix: use continue and not break if area == 0 in lic8
break doesn't check any other points, continue will skip this iteration and start next iteration

fixes slight bug introduced in previous commit 50514e502240b3c00c07e21fd7aa7f4c76d40cb7

lic/src/main/java/com/group7/LaunchInterceptor.java
@@ -238,7 +238,7 @@ public class LaunchInterceptor {
                     if (maxLength >= this.PARAMETERS.RADIUS1) {
                         return true; // Points are not within the radius
                     }
-                    break;
+                    continue;
                 }
 
                 // Calculate the circumcircle radius using the formula from